a33160e04e4963025f9842b4dbcd5e5f42c704ed
AntoDelVecchio <antodelvecchio9@gmail.com>
2020-12-12 00:34:57 -0300

    modificacion de dependencia de fontawesome instalda; cambio de rutas en App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import './App.scss';
+import React from "react";
+import "./styles/styles.scss";
+// import Header from "./components/Header";
+import Fullpage from "./components/Fullpage";
 
 function App() {
   return (
     <div className="App">
+      {/* <Header /> */}
+      <Fullpage />
     </div>
   );
 }

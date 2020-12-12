87e3cfb169a30950dc11de85308fbb0a37e9baa5
AntoDelVecchio <antodelvecchio9@gmail.com>
2020-12-12 00:31:15 -0300

    agregando archivos con la informacion necesaria acerca de mis proyectos y skills

---
+++ src/data.js
@@ -0,0 +1,59 @@
+import htmlIcon from "./images/html.png";
+import cssIcon from "./images/css.png";
+import jsIcon from "./images/javascript.png";
+import reactIcon from "./images/react.svg";
+import gitIcon from "./images/git.png";
+import sassIcon from "./images/sass.svg";
+import bootstrapIcon from "./images/bootstrap.svg";
+import foundationIcon from "./images/foundation.png";
+
+export const skillsData = [
+  {
+    name: "html 5",
+    image: htmlIcon,
+    slug: "html 5",
+    status: 3
+  },
+  {
+    name: "css 3",
+    image: cssIcon,
+    slug: "css 3",
+    status: 3
+  },
+  {
+    name: "javascript",
+    image: jsIcon,
+    slug: "javascript",
+    status: 2
+  },
+  {
+    name: "react",
+    image: reactIcon,
+    slug: "react",
+    status: 1
+  },
+  {
+    name: "git",
+    image: gitIcon,
+    slug: "git",
+    status: 2
+  },
+  {
+    name: "sass",
+    image: sassIcon,
+    slug: "sass",
+    status: 2
+  },
+  {
+    name: "bootstrap",
+    image: bootstrapIcon,
+    slug: "bootstrap",
+    status: 3
+  },
+  {
+    name: "foundation",
+    image: foundationIcon,
+    slug: "foundation",
+    status: 2
+  }
+];
---
+++ src/portfolioData.js
@@ -0,0 +1,76 @@
+import project1 from "./images/comenzandoFotografia.png";
+import project2 from "./images/mundoDetox.png";
+import project3 from  "./images/musicPlease.png";
+import project4 from "./images/carteleraBootstrap.png";
+import project5 from "./images/tienda.png";
+import project6 from "./images/socialNetwork.png";
+import project7 from "./images/travelBlog.png";
+import project8 from "./images/podcast.png";
+import project9 from "./images/GIFOS.png";
+
+export const portfolioData = [
+  {
+    link:
+      "https://antodelvecchio.github.io/Proyecto-responsive-design-fotografias/",
+    photo: project1,
+    slug: "Comenzando con la fotografia",
+    title: "Photography blog",
+    txt: "Using CSS and responsive web design"
+  },
+  {
+    link: "https://antodelvecchio.github.io/Blog-con-Foundation/",
+    photo: project2,
+    slug: "Mundo detox",
+    title: "Cooking blog",
+    txt: "Using Foundation framework"
+  },
+  {
+    link: "https://antodelvecchio.github.io/Blog-con-Bootstrap/",
+    photo: project3,
+    slug: "Music please",
+    title: "Music blog",
+    txt: "Using Bootstrap framework"
+  },
+  {
+    link: "https://antodelvecchio.github.io/Cartelera_cine/",
+    photo: project4,
+    slug: "Cartelera de cine",
+    title: "Cinema listings",
+    txt: "Using Bootstrap framework"
+  },
+  {
+    link: "https://antodelvecchio.github.io/E-store/",
+    photo: project5,
+    slug: "Tienda online",
+    title: "E-store",
+    txt: "Using Foundation Framework"
+  },
+  {
+    link: "https://antodelvecchio.github.io/SocialNetwork/",
+    photo: project6,
+    slug: "Social network",
+    title: "Social network",
+    txt: "Using Materialize framework"
+  },
+  {
+    link: "https://antodelvecchio.github.io/Blog-con-Materialize/",
+    photo: project7,
+    slug: "Travel blog",
+    title: "Travel blog",
+    txt: "Using Materialize framework"
+  },
+  {
+    link: "https://antodelvecchio.github.io/Podcast_Project/",
+    photo: project8,
+    slug: "Podcast",
+    title: "Podcast",
+    txt: "Using SASS and responsive web design"
+  },
+  {
+    link: "https://antodelvecchio.github.io/Gifos-Project/",
+    photo: project9,
+    slug: "Gifos",
+    title: "Gifs",
+    txt: "Using javascript"
+  }
+];

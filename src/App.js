87922030e86be2ea7fc363dda425ecc5399ef717
AntoDelVecchio <antodelvecchio9@gmail.com>
2020-12-18 18:34:59 -0300

    borrando header no usado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./styles/styles.scss";
-// import Header from "./components/Header";
 import Fullpage from "./components/Fullpage";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
       <Fullpage />
     </div>
   );

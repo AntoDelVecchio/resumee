938fb37dcb6f078ea9739fcaad17c871a1814e24
AntoDelVecchio <antodelvecchio9@gmail.com>
2020-12-18 18:28:38 -0300

    cambiando imagenes de la seccion portfolio

--- src/portfolioData.js
+++ src/portfolioData.js
@@ -1,12 +1,14 @@
-import project1 from "./images/comenzandoFotografia.png";
-import project2 from "./images/mundoDetox.png";
-import project3 from  "./images/musicPlease.png";
-import project4 from "./images/carteleraBootstrap.png";
-import project5 from "./images/tienda.png";
-import project6 from "./images/socialNetwork.png";
-import project7 from "./images/travelBlog.png";
-import project8 from "./images/podcast.png";
-import project9 from "./images/GIFOS.png";
+import project1 from "./images/projectsScreen/01-photography.png";
+import project2 from "./images/projectsScreen/02-musicBlog.png";
+import project3 from  "./images/projectsScreen/03-gastronomyBlog.png";
+// import project4 from "./images/projectsScreen/carteleraBootstrap.png";
+// import project5 from "./images/projectsScreen/tienda.png";
+// import project6 from "./images/projectsScreen/socialNetwork.png";
+import project7 from "./images/projectsScreen/04-travelBlog.png";
+import project8 from "./images/projectsScreen/07-podcast.png";
+import project9 from "./images/projectsScreen/08-gifos.png";
+import project10 from "./images/projectsScreen/09-reservaAlojamientos.png";
+import project11 from "./images/projectsScreen/10-rewardsStore.png";
 
 export const portfolioData = [
   {
@@ -15,62 +17,76 @@ export const portfolioData = [
     photo: project1,
     slug: "Comenzando con la fotografia",
     title: "Photography blog",
-    txt: "Using CSS and responsive web design"
-  },
-  {
-    link: "https://antodelvecchio.github.io/Blog-con-Foundation/",
-    photo: project2,
-    slug: "Mundo detox",
-    title: "Cooking blog",
-    txt: "Using Foundation framework"
+    txt: "Using HTML, CSS & responsive web design."
   },
   {
     link: "https://antodelvecchio.github.io/Blog-con-Bootstrap/",
-    photo: project3,
+    photo: project2,
     slug: "Music please",
     title: "Music blog",
-    txt: "Using Bootstrap framework"
+    txt: "Using HTML & Bootstrap framework"
   },
   {
-    link: "https://antodelvecchio.github.io/Cartelera_cine/",
-    photo: project4,
-    slug: "Cartelera de cine",
-    title: "Cinema listings",
-    txt: "Using Bootstrap framework"
+    link: "https://antodelvecchio.github.io/Blog-con-Foundation/",
+    photo: project3,
+    slug: "Mundo detox",
+    title: "Cooking blog",
+    txt: "Using HTML & Foundation framework."
   },
-  {
-    link: "https://antodelvecchio.github.io/E-store/",
-    photo: project5,
-    slug: "Tienda online",
-    title: "E-store",
-    txt: "Using Foundation Framework"
-  },
-  {
-    link: "https://antodelvecchio.github.io/SocialNetwork/",
-    photo: project6,
-    slug: "Social network",
-    title: "Social network",
-    txt: "Using Materialize framework"
-  },
+  // {
+  //   link: "https://antodelvecchio.github.io/Cartelera_cine/",
+  //   photo: project4,
+  //   slug: "Cartelera de cine",
+  //   title: "Cinema listings",
+  //   txt: "Using Bootstrap framework"
+  // },
+  // {
+  //   link: "https://antodelvecchio.github.io/E-store/",
+  //   photo: project5,
+  //   slug: "Tienda online",
+  //   title: "E-store",
+  //   txt: "Using Foundation Framework"
+  // },
+  // {
+  //   link: "https://antodelvecchio.github.io/SocialNetwork/",
+  //   photo: project6,
+  //   slug: "Social network",
+  //   title: "Social network",
+  //   txt: "Using Materialize framework"
+  // },
   {
     link: "https://antodelvecchio.github.io/Blog-con-Materialize/",
     photo: project7,
     slug: "Travel blog",
     title: "Travel blog",
-    txt: "Using Materialize framework"
+    txt: "Using HTML &  Materialize framework."
   },
   {
     link: "https://antodelvecchio.github.io/Podcast_Project/",
     photo: project8,
     slug: "Podcast",
     title: "Podcast",
-    txt: "Using SASS and responsive web design"
+    txt: "Using HTML, CSS, SASS & responsive web design."
   },
   {
     link: "https://antodelvecchio.github.io/Gifos-Project/",
     photo: project9,
     slug: "Gifos",
     title: "Gifs",
-    txt: "Using javascript"
+    txt: "Using Javascript."
+  },
+  {
+    link: "https://antodelvecchio.github.io/reserva-de-alojamientos/",
+    photo: project10,
+    slug: "Reserva de alojamientos",
+    title: "Book Your Room",
+    txt: "Using React."
+  },
+  {
+    link: "https://antodelvecchio.github.io/store-antonella-del-vecchio/",
+    photo: project11,
+    slug: "Rewards Store",
+    title: "Rewards Store",
+    txt: "Using React with hooks."
   }
 ];

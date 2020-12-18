af8ea01fa3c4cac6e1dfaa0240f4eebd808984c2
AntoDelVecchio <antodelvecchio9@gmail.com>
2020-12-18 18:33:41 -0300

    agregando texto personalizado

--- src/components/main/About.jsx
+++ src/components/main/About.jsx
@@ -5,12 +5,24 @@ function About(props) {
     <section id="about">
       <h2 className="title-section">about me</h2>
       <p className="p about-text">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, error
-        nobis amet quidem minus culpa eveniet reiciendis doloremque quibusdam.
+        Hi!! My name is Antonella - front-end developer in progress from Mendoza, Argentina.
+        <br />
+        I'm always interested in learning new things.
       </p>
       <p className="p about-text">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque hic
-        veritatis ad cumque dolor nesciunt eos amet?.
+        A short time ago I got my degree in tourism business management. Also, I
+        introduced myself to the world of web development for fun and curiosity.
+        <br />
+        Right now I wish to continue learning about this and become a
+        professional front-end developer.
+      </p>
+      <p className="p about-text">
+        I've been taking front-end developer courses at Next U and also recently
+        at Acamica on a scholarship of IT and Software Development Globant
+        company.
+      </p>
+      <p className="p about-text">
+        I can't wait to carry out excellent projects with wonderful people.
       </p>
     </section>
   );
